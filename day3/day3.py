ba14004e48007b0460f104bba62adf96ab1f0d0d
Olivier <oliviervandeneede@hotmail.com>
2018-12-03 09:48:31 +0100

    Day 3

---
+++ day3/day3.py
@@ -0,0 +1,51 @@
+import re
+
+
+class fabric:
+    def __init__(self, groups):
+        self.id = groups[0]
+        self.x = int(groups[1])
+        self.y = int(groups[2])
+        self.w = int(groups[3])
+        self.h = int(groups[4])
+        self.intact = True
+
+    def in_range(self, i, j):
+        return i > self.x and i <= self.x + self.w and self.y < j <= self.y + self.h
+
+    def __str__(self):
+        return "#{} @ {},{}: {}x{}".format(self.id, self.x, self.y, self.w, self.h)
+
+
+# input = open("test.txt").read().splitlines()
+input = open("input.txt").read().splitlines()
+
+pattern = "#(\d+) @ (\d+),(\d+): (\d+)x(\d+)"
+
+claims = []
+
+for line in input:
+    m = re.match(pattern, line)
+    claims.append(fabric(m.groups()))
+
+total = 0
+
+for i in range(1000):
+    for j in range(1000):
+        count = 0
+        for claim in claims:
+            if claim.in_range(i, j):
+                count += 1
+
+        if count >= 2:
+            total += 1
+            for claim in claims:
+                if claim.in_range(i, j) and claim.intact:
+                    claim.intact = False
+
+print("Total: {}".format(total))
+
+for claim in claims:
+    if claim.intact:
+        print("Claim {} is intact.".format(claim.id))
+        exit(0)
