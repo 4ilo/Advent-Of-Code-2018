f633ddc7a5655bb64eb0716943c9d52d164f6b36
Olivier <oliviervandeneede@hotmail.com>
2018-12-03 10:00:28 +0100

    day3 cleanup

--- day3/day3.py
+++ day3/day3.py
@@ -1,7 +1,7 @@
 import re
 
 
-class fabric:
+class Fabric:
     def __init__(self, groups):
         self.id = groups[0]
         self.x = int(groups[1])
@@ -11,41 +11,42 @@ class fabric:
         self.intact = True
 
     def in_range(self, i, j):
-        return i > self.x and i <= self.x + self.w and self.y < j <= self.y + self.h
+        return self.x < i <= self.x + self.w and self.y < j <= self.y + self.h
 
     def __str__(self):
         return "#{} @ {},{}: {}x{}".format(self.id, self.x, self.y, self.w, self.h)
 
 
-# input = open("test.txt").read().splitlines()
-input = open("input.txt").read().splitlines()
+if __name__ == "__main__":
+    # input = open("test.txt").read().splitlines()
+    input = open("input.txt").read().splitlines()
 
-pattern = "#(\d+) @ (\d+),(\d+): (\d+)x(\d+)"
+    pattern = "#(\d+) @ (\d+),(\d+): (\d+)x(\d+)"
 
-claims = []
+    claims = []
 
-for line in input:
-    m = re.match(pattern, line)
-    claims.append(fabric(m.groups()))
+    for line in input:
+        m = re.match(pattern, line)
+        claims.append(Fabric(m.groups()))
 
-total = 0
+    total = 0
 
-for i in range(1000):
-    for j in range(1000):
-        count = 0
-        for claim in claims:
-            if claim.in_range(i, j):
-                count += 1
-
-        if count >= 2:
-            total += 1
+    for i in range(1000):
+        for j in range(1000):
+            count = 0
             for claim in claims:
-                if claim.in_range(i, j) and claim.intact:
-                    claim.intact = False
+                if claim.in_range(i, j):
+                    count += 1
 
-print("Total: {}".format(total))
+            if count >= 2:
+                total += 1
+                for claim in claims:
+                    if claim.in_range(i, j) and claim.intact:
+                        claim.intact = False
 
-for claim in claims:
-    if claim.intact:
-        print("Claim {} is intact.".format(claim.id))
-        exit(0)
+    print("Total: {}".format(total))
+
+    for claim in claims:
+        if claim.intact:
+            print("Claim {} is intact.".format(claim.id))
+            exit(0)
